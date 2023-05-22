e140088d5c880c992cc71e7c4eaef19cc08ccf57
Louis Claremont <louisclaremont@MASTUDENT-Louis-Claremont-C17GMB3FQ6L4.local>
2023-05-22 23:12:00 +0100

    updating final frame visual output

--- app.rb
+++ app.rb
@@ -17,17 +17,33 @@ class Application < Sinatra::Base
   end
 
   post '/scorecard' do
-    roll1 = params[:roll1].to_i
-    roll2 = params[:roll2].to_i
-    if roll1 == 10
-      frame = Frame.new([10])
-    else
-      frame = Frame.new([roll1, roll2])
-    end
+    # roll1 = params[:roll1].to_i
+    # roll2 = params[:roll2].to_i
+    # if roll1 == 10
+    #   frame = Frame.new([10])
+    # else
+    #   frame = Frame.new([roll1, roll2])
+    # end
+    frame = construct_frame
     game.add_frame(frame)
 
     @frames = game.frames
     @game = game
     return erb(:scorecard)
+  end
+
+  private
+
+  def construct_frame
+    roll1 = params[:roll1].to_i
+    roll2 = params[:roll2].to_i
+    roll3 = params[:roll3]
+    if roll1 == 10 && roll3.nil?
+      frame = Frame.new([10])
+    elsif roll3.nil?
+      frame = Frame.new([roll1, roll2])
+    else
+      frame = Frame.new([roll1, roll2, roll3.to_i])
+    end
   end
 end
